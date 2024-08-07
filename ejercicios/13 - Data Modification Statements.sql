a22bdc88cfc3ab6c2e267aa7076faae54e77da19
0rellano <orellanojoaquinmarcelo@gmail.com>
2024-08-06 21:58:17 -0300

    clase 13

--- ejercicios/13 - Data Modification Statements.sql
+++ ejercicios/13 - Data Modification Statements.sql
@@ -16,7 +16,9 @@ VALUES(
 	)
 );
 
+
 # 2
+
 INSERT INTO rental(rental_date, customer_id, staff_id, inventory_id)
 VALUES (
 	NOW(), 
@@ -28,6 +30,7 @@ VALUES (
 		WHERE f.title = "ACADEMY DINOSAUR"
 	)
 );
+
 
 # 3
 
@@ -51,6 +54,97 @@ WHERE rating = 'PG-13';
 UPDATE film
 SET release_year = 2005
 WHERE rating = 'R';
+
+
+# 4
+
+UPDATE rental
+SET return_date = NOW()
+WHERE rental_id = (
+	SELECT rental_id 
+	WHERE return_date IS NULL
+	ORDER BY rental_date DESC
+	LIMIT 1
+);
+
+
+# 5
+
+DELETE FROM film WHERE film_id = 1;
+# SQL Error [1451] [23000]: Cannot delete or update a parent row: a foreign key 
+# constraint fails (`sakila`.`film_actor`, CONSTRAINT `fk_film_actor_film` FOREIGN 
+# KEY (`film_id`) REFERENCES `film` (`film_id`) ON DELETE RESTRICT ON UPDATE CASCADE)
+DELETE FROM film_actor WHERE film_id = 1;
+DELETE FROM film_category WHERE film_id = 1;
+DELETE FROM rental WHERE inventory_id IN (
+	SELECT inventory_id
+	FROM inventory
+	WHERE film_id = 1
+);
+DELETE FROM inventory WHERE film_id = 1;
+# you can delete the film
+DELETE FROM film WHERE film_id = 1;
+
+
+# 6
+
+SELECT i.inventory_id 
+FROM inventory i 
+WHERE  i.inventory_id NOT IN (
+	SELECT r.inventory_id 
+	FROM rental r
+	WHERE r.return_date IS NUll
+)
+LIMIT 1;
+# inventory id = 9
+
+INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id)
+VALUES (
+	NOW(), 
+	9,
+	(SELECT MIN(c.customer_id) FROM customer c),
+	(SELECT MIN(s.staff_id)
+		FROM staff s 
+		WHERE store_id = (
+			SELECT i.store_id 
+			FROM inventory i
+			WHERE inventory_id = 9
+		)
+		LIMIT 1
+	)
+);
+
+INSERT INTO payment (customer_id, staff_id, rental_id, amount, payment_date)
+VALUES (
+	(SELECT MIN(c.customer_id) FROM customer c),
+	(SELECT MIN(s.staff_id)
+		FROM staff s 
+		WHERE store_id = (
+			SELECT i.store_id 
+			FROM inventory i
+			WHERE inventory_id = 9
+		)
+		LIMIT 1
+	),
+	(SELECT rental_id 
+		FROM rental 
+		WHERE inventory_id  = 9 
+		AND customer_id = (SELECT MIN(c.customer_id) FROM customer c
+	),
+	666,
+	NOW()
+)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
